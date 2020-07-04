692fb7681aaba09e0da4d8fed567d818eab9dc14
Seungeon-Kim <57888770+Seungeon-Kim@users.noreply.github.com>
2020-07-04 22:28:44 +0900

    5567번_결혼식
    DFS풀이, 2Depth 까지만 탐색

--- pyhton_basic/study_baek/implement/5567_Marry.py
+++ pyhton_basic/study_baek/implement/5567_Marry.py
@@ -4,6 +4,7 @@
 
 # 상근이는 동기들의 친구 관계를 모두 조사한 리스트를 가지고 있다. 이 리스트를 바탕으로 결혼식에 초대할 사람의 수를 구하는 프로그램을 작성하시오.
 
+# [input]
 # 6
 # 5
 # 1 2
@@ -11,35 +12,56 @@
 # 3 4
 # 2 3
 # 4 5
-
+# [output]
 # 3
-def bfs(queues, queue):
-    result = list()
-    while(queue):
-        q = queue.pop(0)
-        if queues[q]:
-            for val in queues[q]:
-                result.append(val)
+
+# 0 1
+# 0 2
+# 2 3 
+# 1 2 
+# 3 4
+
+def initialized_matrix(arr, size):
+    # 0 1 1 0 0 0
+    # 1 0 1 0 0 0
+    # 1 1 0 1 0 0
+    # 0 0 1 0 1 0
+    # 0 0 0 1 0 0
+    # 0 0 0 0 0 0 
+    result = [[0 for _ in range(size)] for _ in range(size)]
+    for pair in arr:
+        x, y = pair[0]-1, pair[1]-1
+        result[x][y] = 1
+        result[y][x] = 1
+    
     return result
 
+def start_dfs(start, mat, size):
+    friends = [start]
+    queue = [start]
+    loop = 0
+
+    while queue:
+        element = queue.pop(0)
+        for index in range(size):
+            if loop == 0 and mat[element][index] == 1:
+                friends.append(index)
+                queue.append(index)
+            elif mat[element][index] == 1:
+                friends.append(index)
+        loop += 1
+    
+    return friends
+
+
+
+
 if __name__ == "__main__":
-    num = int(input())
-    size = int(input())
-    arr = [list(map(int, input().split())) for _ in range(size)]
+    node_size = int(input())
+    link_size = int(input())
+    pair_arr = [list(map(int, input().split())) for _ in range(link_size)]
+    
+    matrix = initialized_matrix(pair_arr, node_size)
+    friends = start_dfs(0, matrix, node_size)
+    print(len(set(friends))-1)
 
-    queues = [list() for _ in range(num)]
-    for element in arr:
-        queues[element[0]-1].append(element[1]-1)
-
-    result = set()
-
-    for my in queues[0]:
-        result.add(my)
-
-
-    tmp = [val for val in range(result)]
-    for fr in tmp:
-        for val in queues[fr]:
-            result.add(val)
-
-    print(result)
